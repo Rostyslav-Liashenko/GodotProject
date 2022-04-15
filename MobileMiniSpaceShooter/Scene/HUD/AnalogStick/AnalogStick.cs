6b10948eeb15e3447c6d0feca59eeaf0fa690ec7
Kivi27 <kivi27072002@gmail.com>
2022-04-15 23:11:45 +0300

    Fix multitouch

--- MobileMiniSpaceShooter/Scene/HUD/AnalogStick/AnalogStick.cs
+++ MobileMiniSpaceShooter/Scene/HUD/AnalogStick/AnalogStick.cs
@@ -2,52 +2,44 @@ using Godot;
 
 public class AnalogStick : Node2D
 {
-    private Sprite topStick;
-    private Area2D hitBox;
+    private Vector2 offset;
+    private TouchScreenButton topStick;
     private bool isDrag;
     
     [Signal] public delegate void MoveTopStick(Vector2 vec);
     
     public override void _Ready()
     {
-        isDrag= false;
-        topStick = GetNode<Sprite>("TopStick");
-        hitBox = GetNode<Area2D>("TopStick/HitBox");
-
-        hitBox.Connect("input_event", this, nameof(OnInputEvent));
+        offset = new Vector2(-48, -48);
+        isDrag = false;
+        topStick = GetNode<TouchScreenButton>("TopStick");
+        topStick.Connect("pressed", this, nameof(OnPressed));
+        topStick.Connect("released", this, nameof(OnReleased));
     }
 
+    public void OnPressed()
+    {
+        isDrag = true;
+    }
+
+    public void OnReleased()
+    {
+        ResetStateStick(); 
+    }
     public override void _Process(float delta)
     {
         if (isDrag)
         {
-            topStick.Position = GetLocalMousePosition();
+            topStick.Position = GetLocalMousePosition() + offset;
             if (topStick.Position.DistanceTo(Vector2.Zero) > 64)
                 topStick.Position = topStick.Position.Normalized() * 63;
         }
         EmitSignal(nameof(MoveTopStick), topStick.Position);
     }
     
-    public override void _UnhandledInput(InputEvent ev)
-    {
-        if (ev is InputEventScreenTouch screenTouch && !screenTouch.Pressed)
-            ResetStateStick();
-    }
-
     private void ResetStateStick()
     {
         isDrag = false;
         topStick.Position = Vector2.Zero;
     }
-    public void OnInputEvent(Viewport viewport, InputEvent ev, int shapeIdx)
-    {
-        if (ev is InputEventScreenTouch screenTouch)
-        {
-            if (screenTouch.Pressed)
-                isDrag = true;
-            else
-                ResetStateStick();
-        }
-    }
-    
 }
